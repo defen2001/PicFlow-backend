01cf9f1ac77dbdd4a82d9a433fc24c0a77155b9a
chendefeng <1257340960@qq.com>
2025-07-31 17:57:22 +0800

    feat：引入腾讯 cos 存储

---
+++ src/main/java/com/defen/picflowbackend/manager/FIleManager.java
@@ -0,0 +1,112 @@
+package com.defen.picflowbackend.manager;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.unit.DataUnit;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.defen.picflowbackend.config.CosClientConfig;
+import com.defen.picflowbackend.exception.BusinessException;
+import com.defen.picflowbackend.exception.ErrorCode;
+import com.defen.picflowbackend.exception.ExceptionUtils;
+import com.defen.picflowbackend.model.dto.file.UploadPictureResult;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.model.ciModel.persistence.ImageInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Service
+public class FIleManager {
+
+    @Resource
+    private CosClientConfig cosClientConfig;
+
+    @Resource
+    private CosManager cosManager;
+
+    /**
+     * 上传图片
+     *
+     * @param multipartFile    文件
+     * @param uploadPathPrefix 上传路径前缀
+     * @return
+     */
+    public UploadPictureResult uploadPicture(MultipartFile multipartFile, String uploadPathPrefix) {
+        validPicture(multipartFile);
+        // 图片上传地址
+        String uuid = RandomUtil.randomString(16);
+        String originalFilename = multipartFile.getOriginalFilename();
+        String uploadFilename = String.format("%s_%s.%s", DateUtil.formatDate(new Date()), uuid, FileUtil.getSuffix(originalFilename));
+        String uploadPath = String.format("/%s/%s", uploadPathPrefix, uploadFilename);
+        File file = null;
+        try {
+            // 创建临时文件
+            file = File.createTempFile(uploadPath, null);
+            multipartFile.transferTo(file);
+            // 上传文件
+            PutObjectResult putObjectResult = cosManager.putPictureObject(uploadPath, file);
+            ImageInfo imageInfo = putObjectResult.getCiUploadResult().getOriginalInfo().getImageInfo();
+            // 封装返回结果
+            UploadPictureResult uploadPictureResult = new UploadPictureResult();
+            int picWidth = imageInfo.getWidth();
+            int picHeight = imageInfo.getHeight();
+            double picScala = NumberUtil.round(picWidth * 1.0 / picHeight, 2).doubleValue();
+            uploadPictureResult.setUrl(cosClientConfig.getHost() + "/" + uploadPath);
+            uploadPictureResult.setPicName(FileUtil.mainName(originalFilename));
+            uploadPictureResult.setPicSize(FileUtil.size(file));
+            uploadPictureResult.setPicWidth(picWidth);
+            uploadPictureResult.setPicHeight(picHeight);
+            uploadPictureResult.setPicScale(picScala);
+            uploadPictureResult.setPicFormat(imageInfo.getFormat());
+            return uploadPictureResult;
+
+        } catch (IOException e) {
+            log.error("图片上传到对象存储失败", e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "图片上传失败");
+        } finally {
+            this.deleteTempFile(file);
+        }
+    }
+
+    /**
+     * 校验文件
+     *
+     * @param multipartFile 文件
+     */
+    public void validPicture(MultipartFile multipartFile) {
+        ExceptionUtils.throwIf(multipartFile == null, ErrorCode.PARAM_ERROR, "文件不能为空");
+        // 1. 校验文件大小
+        long fileSize = multipartFile.getSize();
+        final long ONE_M = 1024 * 1024;
+        ExceptionUtils.throwIf(fileSize > 5 * ONE_M, ErrorCode.PARAM_ERROR, "文件大小不能超过 5M");
+        // 2. 校验文件后缀
+        String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
+        // 允许上传的文件后缀
+        final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpeg", "jpg", "png", "webp");
+        ExceptionUtils.throwIf(!ALLOW_FORMAT_LIST.contains(fileSuffix), ErrorCode.PARAM_ERROR, "文件类型错误");
+
+    }
+
+    /**
+     * 删除临时文件
+     */
+    public void deleteTempFile(File file) {
+        if (file == null) {
+            return;
+        }
+        // 删除临时文件
+        boolean deleteResult = file.delete();
+        if (!deleteResult) {
+            log.error("file delete error, filepath = {}", file.getAbsolutePath());
+        }
+    }
+}
