e657f0cf8fad2004e8dabe6dee02e60bb732ec62
chendefeng <1257340960@qq.com>
2025-08-16 11:22:45 +0800

    fix: 补充文件

---
+++ src/main/java/com/defen/picflowbackend/mapper/SpaceMapper.java
@@ -0,0 +1,18 @@
+package com.defen.picflowbackend.mapper;
+
+import com.defen.picflowbackend.model.entity.Space;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author chendefeng
+* @description 针对表【space(空间)】的数据库操作Mapper
+* @createDate 2025-08-14 10:54:22
+* @Entity com.defen.picflowbackend.model.entity.Space
+*/
+public interface SpaceMapper extends BaseMapper<Space> {
+
+}
+
+
+
+
